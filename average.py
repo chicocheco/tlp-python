d0e10fbbb8d6df1c8b62192d0f74681c04aa9b0c
Stanislav Matas <stanislav.matas@gmail.com>
2022-10-13 19:18:11 +0200

    add average

---
+++ average.py
@@ -0,0 +1,19 @@
+"""
+const int ARRAY_SIZE = 10;
+int gradeArray[ARRAY_SIZE] = {87, 76, 100, 97, 64, 83, 88, 92, 74, 95};
+double sum = 0;
+for (int i = 0; i < ARRAY_SIZE; i++) {
+    sum += gradeArray[i];
+}
+double average = sum / ARRAY_SIZE;
+"""
+
+grades = [87, 76, 100, 97, 64, 83, 88, 92, 74, 95]
+summ = 0
+
+for grade in grades:
+    summ += grade
+
+average = summ / len(grades)
+print('sum:', summ)
+print('average:', average)
